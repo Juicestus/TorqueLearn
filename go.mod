3693b81da489969944b7032f7a59d69c41a4c653
Juicestus <jus@gtsbr.org>
2022-03-30 10:41:19 -0500

    using my new system

---
+++ go.mod
@@ -0,0 +1,3 @@
+module TorqueLearn
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+const (
+	PORT = "8080"
+)
+
+func ServeStaticFiles(port string) {
+	http.Handle("/", http.FileServer(http.Dir("./deploy")))
+
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+
+	http.Handle("/", http.FileServer(http.Dir("./deploy")))
+
+	if err := http.ListenAndServe(":"+PORT, nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
