076627bd79db82ebd28b0c67242281076a604337
Juicestus <jus@gtsbr.org>
2022-03-31 10:31:14 -0500

    Back to the scraping lol. Wasted all of 2nd period
    This reverts commit ef0f35c37f2c2317d228b68809cacafbe7ac95a2.

--- main.go
+++ main.go
@@ -10,21 +10,14 @@ const (
 	URL  = "https://github.com/Juicestus/TorqueLearn"
 )
 
-func ServeStaticFiles(port string) {
+func main() {
+
+	GenerateStaticFiles()
+
 	http.Handle("/", http.FileServer(http.Dir("./deploy")))
 
-	err := http.ListenAndServe(":"+port, nil)
+	err := http.ListenAndServe(":"+PORT, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func main() {
-
-	http.Handle("/", http.FileServer(http.Dir("./deploy")))
-
-	// if err := http.ListenAndServe(":"+PORT, nil); err != nil {
-	// 	log.Fatal(err)
-	// }
-
 }
